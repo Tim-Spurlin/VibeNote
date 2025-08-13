8b42799fb8ddfd6ba04ffd32cf204fd234f18f89
Tim Spurlin <178864942+Tim-Spurlin@users.noreply.github.com>
2025-08-13 13:06:51 -0500

    feat(export): add CSV and structured prompt exporters

--- daemon/src/http_server.cpp
+++ daemon/src/http_server.cpp
@@ -1,1 +1,39 @@
-// Placeholder for HTTP server implementation
+#include <QDateTime>
+#include <QIODevice>
+#include <QString>
+
+#include "store/sqlite_store.h"
+
+namespace exporters {
+void exportCsv(SqliteStore *store, const QDateTime &from, const QDateTime &to,
+               QIODevice *output, QChar delimiter = ',');
+void exportStructuredPrompts(SqliteStore *store, const QDateTime &from,
+                             const QDateTime &to, QIODevice *output,
+                             QChar delimiter = ',');
+} // namespace exporters
+
+class HttpServer {
+public:
+    explicit HttpServer(SqliteStore *store) : store_(store) {}
+
+    void exportNotes(const QString &format, const QDateTime &from,
+                     const QDateTime &to, QIODevice *output,
+                     QChar delimiter = ',');
+
+private:
+    SqliteStore *store_;
+};
+
+void HttpServer::exportNotes(const QString &format, const QDateTime &from,
+                             const QDateTime &to, QIODevice *output,
+                             QChar delimiter) {
+    if (!output) {
+        return;
+    }
+
+    if (format == QLatin1String("csv")) {
+        exporters::exportCsv(store_, from, to, output, delimiter);
+    } else if (format == QLatin1String("structured_prompts")) {
+        exporters::exportStructuredPrompts(store_, from, to, output, delimiter);
+    }
+}
